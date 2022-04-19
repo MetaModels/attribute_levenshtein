4733fb2fc702af1cf7d49b32ec0c26fb0c33358d
Ingolf Steinhardt <git@e-spin.de>
2022-04-19 08:59:46 +0200

    Change autocomplete to vanilla script

--- src/Resources/contao/languages/en/tl_metamodel_filtersetting.php
+++ src/Resources/contao/languages/en/tl_metamodel_filtersetting.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * This file is part of MetaModels/attribute_levensthein.
+ * This file is part of MetaModels/attribute_levenshtein.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,11 +11,19 @@
  * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    MetaModels
- * @subpackage AttributeLevensthein
+ * @subpackage AttributeLevenshtein
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2019 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_levensthein/blob/master/LICENSE LGPL-3.0
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2022 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_levenshtein/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['typenames']['levensthein'] = 'Levenshtein assisted search';
+
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['levenshtein_autocomplete'][0] = 'Activate autocomplete';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['levenshtein_autocomplete'][1] =
+    'You can activate the autocomplete at search field.';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['levenshtein_minChar'][0]      = 'Min. characters for autocomplete';
+$GLOBALS['TL_LANG']['tl_metamodel_filtersetting']['levenshtein_minChar'][1]      =
+    'Number of characters from which the autocomplete should work.';
