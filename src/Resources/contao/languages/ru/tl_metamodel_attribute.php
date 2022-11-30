4435b111433158ba80635f282e96e68c3489df7c
Mini Model <minimodel@metamodel.me>
2022-11-30 09:08:00 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/ru/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/ru/tl_metamodel_attribute.php
@@ -1,21 +1,29 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_levenshtein.
+ *
+ * (c) 2012-2022 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ * Last-updated: 2022-11-30T08:07:46+00:00
  *
- * @link https://www.transifex.com/signup/
- * @link https://www.transifex.com/projects/p/$$project$$/language/$$lang$$/
- *
- * last-updated: 2018-07-06T21:18:30+02:00
+ * @copyright 2012-2022 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_levenshtein/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
 
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_attributes']['0']          = 'Атрибуты для индекса';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_attributes']['1']          = 'Выберите все атрибуты, которые должны быть проиндексированы.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_distance']['0']            = 'Максимальное расстояние Левенштейна';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_distance']['1']            = 'Выберите максимальное расстояние для алгоритма Левенштейна.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_distance_distance']['0']   = 'Допустимое расстояние';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['levensthein_distance_wordlength']['0'] = 'Минимальная длина слова';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['rebuild_levensthein']                  = 'Перестроить индекс поиска.';
